0ec8f1ae6aa3d3866f1a0f2f56b6f023ceb4d798
jihanv <hedjmny@gmail.com>
2025-06-14 20:54:50 +0900

    Add user to database in the script

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 const mongoose = require("mongoose")
 
 const User = require("./User")
-//1. Connect to a database
-const mongoURI = "mongodb://localhost:27017";
+
+//1. Connect to a database, if this database does not exist, it will create it
+const mongoURI = "mongodb://localhost/testDB";
 async function connectToMongo() {
-  try {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(mongoURI);   
-    console.log('Mongo connected');
-  } catch (err) {
-    console.error('Connection error:', err);
-    process.exit(1);
-  }
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(mongoURI);
+        console.log('Mongo connected');
+    } catch (err) {
+        console.error('Connection error:', err);
+        process.exit(1);
+    }
 }
 
 connectToMongo()
 
+//Create a user
+const user = new User({name: "Kyle", age: 26})
+
+//Add user to database
+user.save()
