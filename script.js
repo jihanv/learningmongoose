7496a2a8b032e09f3cb9c7951426a4943f0e8985
jihanv <hedjmny@gmail.com>
2025-06-14 20:27:39 +0900

    Connect to database

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const mongoose = require("mongoose")
+
+
+//2. Connect to a database
+const mongoURI = "mongodb://localhost:27017";
+async function connectToMongo() {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(mongoURI);   
+    console.log('Mongo connected');
+  } catch (err) {
+    console.error('Connection error:', err);
+    process.exit(1);
+  }
+}
+
+connectToMongo()
