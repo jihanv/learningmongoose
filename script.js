6113f706426125c73793ab54ed245566d54fa501
jihanv <hedjmny@gmail.com>
2025-06-15 12:23:51 +0900

    Modigy connectToMongo

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ async function connectToMongo() {
     try {
         mongoose.set('strictQuery', false);
         await mongoose.connect(mongoURI);
-        console.log('Mongo connected');
+        const connection = mongoose.connection;
+        connection.on("connected", () => {
+            console.log("MongoDB Connected successfully.");
+        });
+
+        // Event listener for connection errors
+        connection.on("error", (err) => {
+            console.log("MongoDB connection error. Please make sure MongoDB is running.", err);
+        });
     } catch (err) {
         console.error('Connection error:', err);
         process.exit(1);
@@ -22,7 +30,9 @@ async function run() {
     // user.save();
 
     //Alternative way to create a user
-    const user = await User.create({name: "John", age: 22})
+    const user = await User.create({ name: "Liam", age: 25 })
+
+    console.log("Run!")
 }
 
 
